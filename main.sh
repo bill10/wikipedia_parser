4c443fab026487c24996de6f1e54a4521968d4c0
bill10 <billanson10@gmail.com>
2017-02-17 13:55:11 -0500

    use miniconda to create virtualenv

--- main.sh
+++ main.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # This is to be run on every working node
 
-# load necessary modules
-module load python/2.7
-module load lapack
-module load gcc
+# Install miniconda3
+bash Miniconda3-latest-Linux-x86_64.sh -b
 
-# untar and activate virtual environment
-tar -xzf virtenv.tar.gz
-source ./virtenv/bin/activate
+# Activate virtual environment
+~/miniconda3/bin/conda create -y -n myenv python
+source ~/miniconda3/bin/activate myenv
+
+# Install python packages
+pip install mwxml
+conda install -y pandas
 
 # Run python script
-./virtenv/bin/python2.7 wikipedia_osg.py $1
+python wikipedia_osg.py $1
 
 # deactivate virtual environment
-deactivate
+source ~/miniconda3/bin/deactivate myenv
--- wikipedia_osg.py
+++ wikipedia_osg.py
@@ -3,10 +3,27 @@ import sys
 import subprocess
 import mwxml
 import pandas as pd
-from datetime import datetime as time
+import time
 
+class Stopwatch:
+    start_time=None
+    def go(self,msg=''):
+        if msg:
+            print msg,
+        self.start_time=time.time()
+        sys.stdout.flush()
+    def stop(self,msg=''):
+        if msg:
+            print "{}: {} seconds".format(msg,time.time()-self.start_time)
+        else:
+            print "Elapsed time: {} seconds".format(time.time()-self.start_time)
+        sys.stdout.flush()
+    def check(self):
+        return time.time()-self.start_time
 
-def parser(infile,outfile,namespace,page_titles,limit):
+tic=Stopwatch()
+
+def parser(infile,outfile,namespace,page_titles,limit=None):
     '''
     parse the wikipedia.xml file into csv
 
@@ -19,8 +36,6 @@ def parser(infile,outfile,namespace,page_titles,limit):
     '''
 
     i = 0
-    time_start = time.now()
-
     dump = mwxml.Dump.from_file(infile)
     title=[]
     byte=[]
@@ -48,8 +63,6 @@ def parser(infile,outfile,namespace,page_titles,limit):
         i = i + 1
     df  = pd.DataFrame({'title':title,'time':timestamp,'user':user,'byte':byte})
     df.to_csv(outfile, sep='\t', index=False)
-    time_stop = time.now()
-    print('Time elapsed = %s' % (time_stop - time_start))
 
 
 def main():
@@ -61,11 +74,19 @@ def main():
     base_url = 'https://dumps.wikimedia.org/enwiki/20161201/'
     filename=sys.argv[1]
     url = base_url+filename
+    tic.go('Downloading {}...'.format(filename))
     subprocess.call(["wget", url])
+    tic.stop()
+    tic.go('Decompresing...')
     subprocess.call(["7z", "e", filename])
+    tic.stop()
+    tic.go('Parsing...')
     infile=filename[:-3]
     outfile=filename[:-3]+'.tsv'
-    parser(infile,outfile,namespace,titles,limit=100)
+    parser(infile,outfile,namespace,titles)
+    tic.stop()
+    subprocess.call(["rm", filename])
+    subprocess.call(["rm", infile])
 
 if __name__ == '__main__':
 	main()
