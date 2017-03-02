e3e6ae33493b55c9232390a0e4542d47995f01cd
bill10 <bill@Bills-MacBook-Pro.local>
2017-03-02 01:23:36 -0500

    adapted for turing computing

--- wikipedia_turing.py
+++ wikipedia_turing.py
@@ -4,6 +4,7 @@ import subprocess
 import mwxml
 import pandas as pd
 import time
+from multiprocessing import Pool, cpu_count
 
 class Stopwatch:
     start_time=None
@@ -64,29 +65,34 @@ def parser(infile,outfile,namespace,page_titles,limit=None):
     df  = pd.DataFrame({'title':title,'time':timestamp,'user':user,'byte':byte})
     df.to_csv(outfile, sep='\t', index=False)
 
-
-def main():
+def mapper(filename):
     namespace=1
-    titles=set()
-    with open('page_titles.txt') as f:
-        for l in f:
-            titles.add(l.strip('\n"'))
     base_url = 'https://dumps.wikimedia.org/enwiki/20161201/'
-    filename=sys.argv[1]
     url = base_url+filename
     tic.go('Downloading {}...'.format(filename))
     subprocess.call(["wget", url])
     tic.stop()
     tic.go('Decompresing...')
-    subprocess.call(["./p7zip/bin/7z", "e", filename])
+    subprocess.call(["7z", "e", filename])
     tic.stop()
     tic.go('Parsing...')
     infile=filename[:-3]
     outfile=filename+'.tsv'
     parser(infile,outfile,namespace,titles)
     tic.stop()
-    #subprocess.call(["rm", filename])
-    #subprocess.call(["rm", infile])
+    subprocess.call(["rm", filename])
+    subprocess.call(["rm", infile])
 
 if __name__ == '__main__':
-    main()
+    titles=set()
+    with open('page_titles.txt') as f:
+        for l in f:
+            titles.add(l.strip('\n"'))
+    dumps=[]
+    with open('all_dumps.txt') as f:
+        for l in f:
+            dumps.append(l.strip('\n'))
+    pool=Pool(cpu_count())
+    pool.map(mapper,dumps)
+    pool.join()
+    pool.close()
